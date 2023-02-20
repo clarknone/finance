228e2d30433d2fc83964064b20ed99df928218b0
carla <carla@mail.com>
2023-02-20 22:02:03 +0100

    db setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { SysModule } from './sys/sys.module';
 import { SystemModule } from './system/system.module';
-import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, SysModule, SystemModule, UserModule],
+  imports: [AuthModule,  SystemModule],
   controllers: [AppController],
   providers: [AppService],
 })
